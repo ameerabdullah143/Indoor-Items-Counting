15ad98404c4a2386699369a12bacab1f7bc8bcaf
ameerabdullah143 <57149787+ameerabdullah143@users.noreply.github.com>
2023-09-07 21:12:35 +0500

    drawing generated

--- Goodwill_indoor.py
+++ Goodwill_indoor.py
@@ -3,7 +3,10 @@ import json
 import cv2
 import numpy as np
 
+global width
+global height
 
+FPS = 15
 donation_zones_path = 'ADC-RIVER.OAKS-MACO2.jpg.json'
 data_path = 'h264__2023-09-03_15-18-01-ADC-RIVER.OAKS-MACO2_09042023.json'
 
@@ -17,13 +20,13 @@ def get_donation_zones(path):
     
     donation_zones = []
     for i in data['instances']:
-        print(i)
         p = np.array(i['points']).astype(int)
         p = np.reshape(p,((p.shape[0])//2,2))
         if i['className']=='DonationZone':
           donation_zones.append(p)
     
         # cv2.polylines(img, [p], True, (255,0,0),3)
+        
     global height
     global width
     height,width = data['metadata']['height'],data['metadata']['width']
@@ -32,7 +35,7 @@ def get_donation_zones(path):
     # Closing file=
     f.close()
     
-    return donation_zones
+    return donation_zones,height,width
 
 def parse_string(s):
   items = s.split('|')
@@ -47,6 +50,17 @@ def parse_string(s):
 
   return cls,int(t_id),x1,y1,cx,cy,float(conf) 
 
+def get_coord(s):
+  items = s.split('|')
+
+  cls, t_id, _, x1_p, y1_p, x2_p, y2_p, conf = items
+
+  x1, y1 = int(int(x1_p) * width / 10000), int(int(y1_p) * height / 10000)
+  x2, y2 = int(int(x2_p) * width / 10000), int(int(y2_p) * height / 10000)
+  
+  return (x1,y1),(x2,y2)
+
+
 def update_counter(counted_items,arm_items):
     c = 0
     for key,item in arm_items.items():        
@@ -60,9 +74,14 @@ def update_counter(counted_items,arm_items):
             counted_items.discard(key)
 
 
-donation_zones = get_donation_zones(donation_zones_path)
+donation_zones,height,width = get_donation_zones(donation_zones_path)
+blank_image = np.ones((height,width,3),np.uint8)
 
+for zone in donation_zones:
+    cv2.polylines(blank_image, [zone], True, (0,255,0),3)
 
+# out = cv2.VideoWriter('out.avi',cv2.VideoWriter_fourcc('M','J','P','G'),FPS,(height,width))
+video = cv2.VideoWriter('out.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 15,(width,height))
 
 # Opening JSON file
 f = open(data_path)
@@ -74,27 +93,45 @@ data = json.load(f)['messages']
 
 arm_items = {}
 counted_items = set()
-i = 0
 for frame in data:
+  frame_image = blank_image.copy()
+  
+  cv2.putText(frame_image, "Frame Counter: "+str(frame['id']), (50,50), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255))  
+  cv2.putText(frame_image, "Time Stamp: "+str(frame['ts']), (900,50), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255))  
     
   # processing each frame
   for i in frame['objects']:
       
-        obj = parse_string(i)
-        if obj[0]== 'Arm_Item':
-            if obj[1] in arm_items:
-                arm_items[obj[1]] = [arm_items[obj[1]][0],(obj[4],obj[5])]
+    obj = parse_string(i)
+    s,e = get_coord(i)
+    if obj[0]== 'Arm_Item':
+        cv2.rectangle(frame_image,s,e,(255,255,0),2)
+        if obj[1] in arm_items:
+            arm_items[obj[1]] = [arm_items[obj[1]][0],(obj[4],obj[5])]
 
-            else:
-                arm_items[obj[1]] = [(obj[4],obj[5]),(obj[4],obj[5])]
-            
+        else:
+            arm_items[obj[1]] = [(obj[4],obj[5]),(obj[4],obj[5])]
+    elif obj[0]=='Cart':
+        cv2.rectangle(frame_image,s,e,(0,0,255),2)
+        
+    else:
+        cv2.rectangle(frame_image,s,e,(0,234,255),2)
+        
        
   update_counter(counted_items, arm_items)
+  cv2.putText(frame_image, "Item Counter: "+str(len(counted_items)), (50,100), cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255))  
+  video.write(frame_image)
+  # cv2.imshow("Result",frame_image)
+  
+  # cv2.waitKey(0)
+  
   
               
 
-  # print(len(counted_items))
-  
-
+  print(frame['id'],len(counted_items))
+  # import time
+  # time.sleep(0.100)
+video.release()
+cv2.destroyAllWindows()
 print("Total items:",len(counted_items))
     
